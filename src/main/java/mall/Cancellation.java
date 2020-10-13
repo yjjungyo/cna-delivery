c5883f8797d0756a0ada83e6918d2fc2ad8a1d71
yjjungyo <yjjungyo@gmail.com>
2020-10-13 12:41:17 +0900

    1st commit

---
+++ src/main/java/mall/Cancellation.java
@@ -0,0 +1,52 @@
+package mall;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Cancellation_table")
+public class Cancellation {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.TABLE)
+    private Long id;
+    private Long orderId;
+    private String status;
+
+    @PostPersist
+    public void onPostPersist(){
+        DeliveryCancelled deliveryCancelled = new DeliveryCancelled();
+        BeanUtils.copyProperties(this, deliveryCancelled);
+        deliveryCancelled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+}
---
+++ src/main/java/mall/CancellationRepository.java
@@ -0,0 +1,8 @@
+package mall;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
+
+
+}
